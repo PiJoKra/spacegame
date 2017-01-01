c0585ef097f37ad5d4cb1e9a5bb46e78a6fea89c
PiJoKra <PiJoKra@users.noreply.github.com>
2017-01-01 17:27:05 +0100

    replaced old spacegame by the newer version (prior spacegame2.asm)

--- spacegame.asm
+++ spacegame.asm
@@ -1,535 +1,196 @@
-	.inesprg 1   ; 1x 16KB PRG code
-	.ineschr 1   ; 1x  8KB CHR data
-	.inesmap 0   ; mapper 0 = NROM, no bank swapping
-	.inesmir 1   ; background mirroring
-
-	.rsset $0000 ;Game related stuff
-
-gamestate .rs 1	;$0000
-GAME_STATE_MENU = $00
-GAME_STATE_PLAYING = $01
-GAME_STATE_DEAD = $02
-
-buttons .rs 1 ;$0001
-BUTTON_A		= %10000000
-BUTTON_B		= %01000000
-BUTTON_SELECT	= %00100000
-BUTTON_START	= %00010000
-BUTTON_UP		= %00001000
-BUTTON_DOWN		= %00000100
-BUTTON_LEFT		= %00000010
-BUTTON_RIGHT	= %00000001
-
+	.inesprg 1
+	.ineschr 1
+	.inesmap 0
+	.inesmir 1
+	
+	.rsset $0000
+	
+gamestate .rs 1
 score .rs 4
+buttons .rs 1
 
-	.rsset $0010 ;Player related stuff
-
-playerSpeed .rs 1 ;$0010
-playerX .rs 1 ;$0011
-playerY .rs 1 ;$0012
-PLAYER_MIN_X = $0A
-PLAYER_MIN_Y = $0A
-PLAYER_MAX_X = $A0
-PLAYER_MAX_Y = $A0
-
-canShoot .rs 1 ;$0013
-CAN_SHOOT_COUNTER = $70
-
-	;Bullets are stored as $xx $yy | $xx $yy | ... from $0020 until $0040
-	;This means that 16 bullets can be stored at the same time
-	;Instead of destroying bullets when they get out of the screen,
-		;they get overwritten once the game runs out of space for new bullets
-	;This means there can be a bullet overflow, but that should be solved by
-		;the canShoot counter
+playerSpeed .rs 1
+playerX .rs 1
+playerY .rs 1
+canShoot .rs 1
 
 
-
+	
+	.bank 1
+	
+	.org $E000
+	.include "spacegame/palette.asm"
+	.include "spacegame/backgroundGame.asm"
+	.include "spacegame/player.asm"
+	
+	.org $FFFA
+	.dw	NMI
+	.dw reset
+	.dw 0
+	
+;==================================================;
+	
+	;NES games MUST have their main code in bank 0,
+	; and on address $8000 or $C000 (you can choose)
 	.bank 0
-	.org $C000	
+	.org $8000
+	.include "spacegame/strings.asm"
+	.include "spacegame/updateScore.asm"
+	.include "spacegame/handleButtons.asm"
+
+;Picture Processing Unit ports
+PPU_CONTROLLER = $2000
+PPU_MASK = $2001
+PPU_STATUS_REGISTER = $2002
+	;after setting PPU_ADDRESS_REGISTER you need to set PPU_SCROLL again as they have a shared internal register. That is why it is best to change PPU_SCROLL at the end. (http://wiki.nesdev.com/w/index.php/PPU_scrolling#Frequent_pitfalls)
+PPU_SCROLL = $2005
+PPU_ADDRESS_REGISTER = $2006
+PPU_DATA = $2007
+
+;PPU Object Attribute Memory
+PPU_OAM_ADDRESS = $2003
+	;PPU_OAM_DATA is not used, as it can cause some weird behavior that I do not fully understand myself.
+	;Instead, data is written to $0200, and than automatically transferred to the PPU. For this last thing to work PPU_OAM_DMA has to be set to $02
+PPU_OAM_DATA = $2004
+	;High bit of OAM. When PPU_OAM_DMA has a value of $XX, the data at addresses $XX00-$XXFF will be read and transferred to the PPU to draw as sprites
+PPU_OAM_DMA = $4014
+
+;Central Processing Unit ports
+CPU_JOYSTICK_1 = $4016
+CPU_JOYSTICK_2 = $4017 ;Shares address with APU FC
+
+;Audio Processing Unit ports
+APU_DELTA_MODULATION_CHANNEL = $4010
+APU_FRAMECOUNTER_CONTROL = $4017 ;Shares address with CPU J2
 	
-RESET:
-	SEI          ; disable IRQs
-	CLD          ; disable decimal mode
-	LDX #$40
-	STX $4017    ; disable APU frame IRQ
-	LDX #$FF
-	TXS          ; Set up stack
-	INX          ; now X = $FF + 1 => 0
-	STX $2000    ; disable NMI
-	STX $2001    ; disable rendering
-	STX $4010    ; disable DMC IRQs
-
-	JSR vBlankWait
-
-clrmem:
-	LDA #$00
-	STA $0000, x
-	STA $0100, x
-	STA $0300, x
-	STA $0400, x
-	STA $0500, x
-	STA $0600, x
-	STA $0700, x
-
-	LDA #$FE
-	STA $0200, x
-
-	INX
-	BNE clrmem
-
-	JSR vBlankWait
-
-LoadPalettes:
-	LDA $2002 ;Reset high/low latch
+reset:
 	
-	;Write #$3F00 as first (background) palette address
-	LDA #$3F
-	STA $2006
-	LDA #$00
-	STA $2006
-
-	;$3F00-$3F0F are for background colors
-	;$3F10-$3F1F are for sprite colors
-	LDX #$00
-	LoadPalettesLoop:
-		LDA Palette, x
-		STA $2007
-		INX
-		CPX #$20
-		BNE LoadPalettesLoop
-
-
-LoadSpaceShipSprite:
-	;$0200-$02FF is sprite data where every 4 bytes is a sprite
-	;First sprite will be the spaceship
-	LDX #$00
-	LoadSpaceShipSpriteLoop:
-		LDA SpaceShipSprite, x
-		STA $0200, x
-		INX
-		CPX #$10
-		BNE LoadSpaceShipSpriteLoop
-
-
-EnableNMIAndSprites:
-	LDA #%10000000 ;Enable NMI
-	STA $2000
-
-	LDA #%00010000 ;Enable sprites
-	STA $2001
+	;clear decimal mode, as it is not supported by NES anyway
+	cld
 	
-InitializeGame:
-	LDA #$80
-	STA playerX
-	STA playerY
-	LDA #$02
-	STA playerSpeed
+	;7th bit of framecounter control is the interupt inhibit flag
+	;Setting it to 1 puts it into a 5-step sequence
+	;TODO:... more information
+	lda %01000000
+	sta APU_FRAMECOUNTER_CONTROL
 	
-	LDA #CAN_SHOOT_COUNTER
-	STA canShoot
+	;Initialize the stack
+	ldx #$FF
+	txs
 	
-;	JMP StopResetAndClearMemory
+	lda #$00
+	sta PPU_CONTROLLER
+	sta PPU_MASK
+	sta PPU_SCROLL
+	sta PPU_SCROLL
+	sta APU_DELTA_MODULATION_CHANNEL
 	
-CreateBackground:
+	jsr waitVBlank
 	
-	LDA $2002
-	LDA #$20
-	STA $2006
-	LDA #$00
-	STA $2006
-	LDX #$00
-	.LoadingHUD:
-		LDA HUD, x
-		STA $2007
+	ldx #$00
+	lda #$00
+clearMemory:
+	sta $0000, x
+	sta $0100, x
+	sta $0300, x
+	sta $0400, x
+	sta $0500, x
+	sta $0600, x
+	sta $0700, x
+	
+	inx
+	bne clearMemory
+	
+	inx ;Overflow x back from FF to 0
+	lda #$FF ;Sprites are hidden if they have a y-value of $EF or $FF
+clearSpriteData:
+	sta $0200, x
+	
+	inx
+	bne clearSpriteData
+	
+	;TODO: fields $01FD-01FF are set in waitVBlank...
+	;That does not happen if the vblank code is put here...
+	;But if you put both the previous waitVBlank and this one in its own container, than the same fields are written to...
+	;Assembly6502... Y SO CONFUSING??...
+	jsr waitVBlank
+	
+loadPalette:
+	lda PPU_STATUS_REGISTER
+	
+	lda #$3F
+	sta PPU_ADDRESS_REGISTER
+	lda #$00
+	sta PPU_ADDRESS_REGISTER
+	
+	ldx #$00
+	loopLoadPalette:
+		lda palette, x
+		sta PPU_DATA
 		
-		INX
-		CPX #$80
-		BNE .LoadingHUD
-	;LDA #$01
-	STA $20
-	;JMP LoadAttributes
-	LDA #$20
-	STA $2006
-	TXA
-	STA $2006
-	;STA $AA
-	;LDX #$00
-	LDX #$00
-	.GeneratingBackground:
-		LDY #$00
-		.GenerateRow:
-			;TYA
-			LDA #$25
-			;STA $20, y
-			;LDA #$18
-			;TXA
-			STA $2007
-			;TXA
-			;STA $20
-			;TYA
-			;STA $21
-			;LDA $20
-			;ADC $21
-			;STA $20, x
-			
-			;LDA #$1
-			;STA ($22)
-			
-			INY
-			CPY #$1B
-			BNE .GenerateRow
-		INX
-		CPX #$20
-		BNE .GeneratingBackground 
-
-LoadAttributes:
-	LDA $2002
-	LDA #$23
-	STA $2006
-	LDA #$C0
-	STA $2006
+		inx
+		cpx #$20
+		bne loopLoadPalette
 	
-	LDX #$00
-	.loop:
-		LDA Attributes, x
-		STA $2007
 		
-		INX
-		CPX #$8
-		BNE .loop
+background:
+	jsr loadBackgroundGame	
 	
-	;LDA #%10010000
-	;STA $2000
 	
-	;LDA #%00011110
-	;STA $2001
+initialisePlayer:
+	jsr resetPlayerVariables
+	
+enableNMI:
+	;First bit enables NMI on every vertical blanking interval
+	;4th bit says that the background tiles should be read from $1000 instead of $0000
+	;http://wiki.nesdev.com/w/index.php/PPU_registers#Controller_.28.242000.29_.3E_write
+	lda #%10010000
+	sta PPU_CONTROLLER
+	
+enableSprites:
+	;First three bits are to emphasise Blue, Green or Red (not used)
+	;4th bit enables sprites
+	;5th bit enables backgrounds
+	;6th bit enables sprites to be shown in the first 8 columns
+	;7th bit enables background to be shown in the first 8 columns
+	;8th bit enables grayscale mode (not used)
+	;http://wiki.nesdev.com/w/index.php/PPU_registers#Mask_.28.242001.29_.3E_write
+	lda #%00011110
+	sta PPU_MASK
 
-RTS
 	
-
-StopResetAndClearMemory: ;Prevent code from reset and clearing of memory to leak through
-	JMP StopResetAndClearMemory
-
-vBlankWait:
-	BIT $2002
-	BPL vBlankWait ;If not ready, repeat
-	RTS
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-ReadInput:
-	LDA #$01
-	STA $4016
-	LDA #$00
-	STA $4016
-
-	LDX #$08
-	ReadControllerLoop:
-		LDA $4016
-		LSR A
-		ROL buttons
-		DEX
-		BNE ReadControllerLoop
-
-	RTS
-
-HandleButtonUp:
-	LDA buttons
-	AND #BUTTON_UP
-	BEQ .rts
+endReset:
+	jmp endReset
 	
-	LDA playerY
-	SEC
-	SBC playerSpeed
-	STA playerY
+waitVBlank:
+	bit PPU_STATUS_REGISTER
+	bpl waitVBlank
+	rts
 	
-	.rts:
-		RTS
-
-HandleButtonRight:
-	LDA buttons
-	AND #BUTTON_RIGHT
-	BEQ .rts
-	
-	LDA playerX
-	CLC
-	ADC playerSpeed
-	STA playerX
-	
-	.rts:
-		RTS
-
-HandleButtonDown:
-	LDA buttons
-	AND #BUTTON_DOWN
-	BEQ .rts
-	
-	LDA playerY
-	CLC
-	ADC playerSpeed
-	STA playerY
-	
-	.rts:
-		RTS
-
-HandleButtonLeft:
-	LDA buttons
-	AND #BUTTON_LEFT
-	BEQ .rts
-	
-	LDA playerX
-	SEC
-	SBC playerSpeed
-	STA playerX
-	
-	.rts:
-		RTS
-		
-HandleButtonShoot:
-
-	LDA canShoot
-	BEQ .shoot
-	SEC
-	SBC #$1
-	STA canShoot
-	RTS
-	
-	.shoot:
-		LDA buttons
-		AND #BUTTON_A
-		BEQ .rts
-		
-		LDA #CAN_SHOOT_COUNTER
-		STA canShoot
-		
-	.rts:
-		RTS
-
-RestrictSpaceShipPositionY:
-	LDA playerY
-	CMP #PLAYER_MIN_Y
-	BCC .restrictYLowerBound
-	CMP #PLAYER_MAX_Y
-	BCS .restrictYHigherBound
-	RTS
-	
-	.restrictYLowerBound:
-		LDA #PLAYER_MIN_Y
-		STA playerY
-		RTS
-	
-	.restrictYHigherBound:
-		LDA #PLAYER_MAX_Y
-		STA playerY
-		RTS
-	
-RestrictSpaceShipPositionX:
-	LDA playerX
-	CMP #PLAYER_MIN_X
-	BCC .restrictXLowerBound
-	CMP #PLAYER_MAX_X
-	BCS .restrictXHigherBound
-	RTS
-	
-	.restrictXLowerBound:
-		LDA #PLAYER_MIN_X
-		STA playerX
-		RTS
-	
-	.restrictXHigherBound:
-		LDA #PLAYER_MAX_X
-		STA playerX
-		RTS
-	
-AllignSpaceShipSprites:
-
-	;Sprites of the spaceship: 
-	;200 x x 203 | 204 x x 207 | 208 x x 20B | 20C x x 20F
-	;First number is y, forth is x
-
-	LDA playerY
-	STA $200
-	STA $204
-	CLC
-	ADC #$8
-	STA $208
-	STA $20C
-	
-	LDA playerX
-	STA $203
-	STA $20B
-	CLC
-	ADC #$8
-	STA $207
-	STA $20F
-	RTS
-	
-UpdateScore:
-	LDX #$3 ;Start with the lowest digit of the score
-	INC score, x ;Increase the lowest digit of the score
-	
-	;Since every digit of the score is saved seperately, we have to check that
-		;not a single digit is over 9, but instead overflows to the next digit
-	
-	SEC ;Set clear bit since we will only subtract here anyway
-	HandleScoreDigits:
-		CPX #$FF ;If we handled the highest digit of the score (score + 0), X will now be $FF ($0 - $1)
-		BEQ .rts
-		
-		LDA score, x
-		CMP #$0A ;If this digit is lower than $A, so $0-$9, we do not have to look any further 
-		BCC .rts
-		
-		SBC #$0A ;If the digit is $A, set it back at $0
-		STA score, x ;Store the new digit
-		DEX ;Decrease X, so the next digit will be updated
-		INC score, x ;Increase that next digit, since the previous one overflowed
-		
-		JMP HandleScoreDigits ;Repeat until no more digits
-		
-	.rts:
-		RTS
-;ShowScore:
-	; +----------------------------+ (28 x -) 
-	 ;| score: xxxx highscore: xxxx|
-	 ;+----------------------------+ (28 x -)
-	
-;	LoadHUDBorderTop:
-;		LDA $2002
-;		LDA #$20
-;		STA $2006
-;		LDA #$00
-;		STA $2006
-		
-		;Draw top border HUD and first part of score
-		; +-----...-------+
-		; + score: 
-;		LDX #$00
-;		.loop:
-;			LDA HUD, x
-;			STA $2007
-;			
-;			INX
-;			CPX #$43
-;			BNE .loop
-;			
-;		;Draw score number
-;		LDX #$00
-;		.loop2:
-;			LDA score, x
-;			STA $2007
-;			
-;			INX
-;			CPX #$4
-;			BNE .loop2
-			
+;==================================================;
 
 NMI:
-	;JMP .test
-	LDA #$00
-	STA $2003       ; set the low byte (00) of the RAM address
-	LDA #$02
-	STA $4014       ; set the high byte (02) of the RAM address, start the transfer
+	lda #$00
+	sta PPU_OAM_ADDRESS
+	lda #$02
+	sta PPU_OAM_DMA
 	
-	JSR AllignSpaceShipSprites
+	jsr readInput
+	jsr repositionPlayer
 	
-	LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
-	STA $2000
-	LDA #%00011110   ; enable sprites, enable background, no clipping on left side
-	STA $2001
+	jsr updateScore
+	jsr updateScoreHUD
 	
+	;Set PPU_SCROLL to 0000 as it gets reset every time PPU_ADDRESS_REGISTER is read
+	lda #$00
+	sta PPU_SCROLL
+	sta PPU_SCROLL
 	
-	LDA #$00        ;;tell the ppu there is no background scrolling
-	STA $2005
-	STA $2005
+	;End execution of NMI
+	rti
+
+;=================================================;
 	
-	JSR ReadInput
-	
-	JSR HandleButtonUp
-	JSR HandleButtonRight
-	JSR HandleButtonDown
-	JSR HandleButtonLeft
-	
-	JSR RestrictSpaceShipPositionY
-	JSR RestrictSpaceShipPositionX
-	
-	;JSR AllignSpaceShipSprites
-	
-	JSR HandleButtonShoot
-	
-	JSR UpdateScore
-	;JSR ShowScore
-	
-	;LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
-	;STA $2000
-	;LDA #%00011110   ; enable sprites, enable background, no clipping on left side
-	;STA $2001
-	
-	;LDA #$00
-	;STA $2005
-	;STA $2005
-	.test:
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-	.bank 1
-    .org $E000
-
-Palette:
-	;Background colors
-	.db $00, $30, $2D, $1F ;White, Gray, Black
-	.db $00, $11, $11, $11
-	.db $00, $11, $11, $11
-	.db $00, $11, $11, $11
-
-	;Sprite colors
-	;First color is always used for transparancy and 
-	;needs to be the same color (last one I am not 100% sure of tho...)
-	.db $00, $0F, $16, $30 ;Black, Red, White
-	.db $00, $11, $21, $30 ;Blue, Light blue, White
-	.db $00, $0F, $0F, $0F
-	.db $00, $0F, $0F, $0F 
-
-SpaceShipSprite:
-	.db $08, $00, $01, $08
-	.db $08, $01, $01, $10
-	.db $10, $10, $01, $08
-	.db $10, $11, $01, $10
-	
-HUD:
-	.db $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $18
-		.db $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $18
-
-	; +---...---+
-	.db $18, $10, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11
-		.db $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $12, $18
-
-	; | score: xxxx
-	.db $18, $13, $18, $32, $22, $2E, $31, $24, $18, $00, $00, $00, $00, $18, $18, $18
-		.db $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $18, $14, $18
-
-	.db $18, $15, $16, $16, $16, $16, $16, $16, $16, $16, $16, $16, $16, $16, $16, $16
-		.db $16, $16, $16, $16, $16, $16, $16, $16, $16, $16, $16, $16, $16, $16, $17, $18
-	
-Attributes:
-	.db %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000
-
-
-
-
-
-
-    .org $FFFA     ;first of the three vectors starts here
-    .dw NMI        ;when an NMI happens (once per frame if enabled) the 
-                     ;processor will jump to the label NMI:
-    .dw RESET      ;when the processor first turns on or is reset, it will jump
-                     ;to the label RESET:
-    .dw 0          ;external interrupt IRQ is not used in this tutorial
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-    .bank 2
-    .org $0000
-    .incbin "spacegame/spacegame.chr"
+	.bank 2
+	.org $0000
+	.incbin "spacegame/spacegame.chr"
